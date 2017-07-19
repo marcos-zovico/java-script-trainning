6c3ad623f5c5a4e11bd4c374506ce1edc4d48855
Marcos <marcosantoniozovico@hotmail.com>
2017-07-19 14:22:22 -0300

    JavaScript Layers

---
+++ JavaScript_Layers/script.js
@@ -0,0 +1,9 @@
+		function changeOver()
+		{
+			document.getElementById("myElement").className = "h1MouseOver";
+		}
+		
+		function changeOut()
+		{
+			document.getElementById("myElement").className = "h1Mouse";
+		}
